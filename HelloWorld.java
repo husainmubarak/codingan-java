cb76dbb7338a1c544b299485928142e1221a9b40
Muhammad Husain Al Mubarak <154283813+husainmubarak@users.noreply.github.com>
2024-09-21 15:13:16 +0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Nama : M Husain Al Mubarak");
+        System.out.println("NIM : 2411016310022");
+
+        System.out.println(4);
+        System.out.println(false);
+    }
+}
